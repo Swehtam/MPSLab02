5e4723796fcef14b7e97ebac8640a802c705c926
samueldemoura <samueldemouramoreira@gmail.com>
2019-03-14 14:07:39 -0300

    Implement User class

--- src/business/model/User.java
+++ src/business/model/User.java
@@ -7,8 +7,25 @@ package business.model;
 
 /**
  *
- * @author aluno
+ * @author samueldemoura
  */
 public class User {
-    
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
