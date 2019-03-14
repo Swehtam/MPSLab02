40739226cb5fa924c57a283f090721d67e2ff486
Swehtam <mathewsalves1@outlook.com>
2019-03-14 14:22:43 -0300

    remoção e adição de usuários implementado

--- src/business/control/UserControl.java
+++ src/business/control/UserControl.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package business.control;
 
-/**
- *
- * @author aluno
- */
+import java.util.Map;
+import java.util.HashMap;
+import business.model.User;
+import util.*;
+
 public class UserControl {
+    public Map<String, User> usuarios = new HashMap();
     
+    public void add(String login, String password) throws UserLoginException{
+        
+        if(usuarios.containsKey(login)){
+            throw new UserLoginException("Usuário já criado");
+        }
+        
+        usuarios.put(login, new User(login,password));
+        System.out.println("Usuário " + login + " adicionado com sucesso!");
+    }
+    
+    public void del(String login) throws UserLoginException{
+        
+        if(!usuarios.containsKey(login)){
+            throw new UserLoginException("Usuário já criado");
+        }
+        
+        usuarios.remove(login);
+        System.out.println("Usuário " + login + " removido com sucesso!");
+    }
 }
