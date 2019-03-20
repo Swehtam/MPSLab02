8b1ec35d2ff17458e9b1aea8c3e4dffcfa9744e4
samueldemoura <samueldemouramoreira@gmail.com>
2019-03-20 17:27:08 -0300

    Implement main

--- src/view/Main.java
+++ src/view/Main.java
@@ -5,9 +5,18 @@
  */
 package view;
 
+import business.control.UserControl;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.UserLoginException;
+import util.UserPasswordException;
+
 /**
  *
- * @author aluno
+ * @author samueldemoura
  */
 public class Main {
 
@@ -15,7 +24,39 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        UserForm userForm = new UserForm(new BufferedReader(new InputStreamReader(System.in)), System.out);
+        UserControl userControl = new UserControl();
+        
+        String[] credentials;
+        System.out.println("1. Register new user\n2. Remove existing user\n3. Exit");
+        int choice;
+        
+        do {
+            try {
+                choice = userForm.getChoice();
+            
+                switch (choice) {
+                    case 1:
+                        credentials = userForm.askForCredentials();
+                        
+                        try {
+                            userControl.add(credentials[0], credentials[1]);
+                        } catch (UserLoginException | UserPasswordException ex) {
+                            System.out.println("ERROR: " + ex.getMessage());
+                        }
+                        
+                        break;
+                    case 2:
+                        throw new UnsupportedOperationException();
+                    default:
+                        System.out.print("Invalid option, please try again.");
+                        break;
+                }
+            } catch (IOException ex) {
+                System.out.println("Failed to get user input.");
+                break;
+            }
+        } while (choice != 3);
     }
     
 }
--- src/view/UserForm.java
+++ src/view/UserForm.java
@@ -5,10 +5,43 @@
  */
 package view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
 /**
  *
- * @author aluno
+ * @author samueldemoura
  */
 public class UserForm {
+    private BufferedReader in;
+    private PrintStream out;
+
+    public UserForm(BufferedReader in, PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
     
+    public int getChoice() throws IOException {
+        out.print("> Input your choice: ");
+        String user_input = in.readLine();
+        
+        try {
+            return Integer.parseInt(user_input);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+    
+    public String[] askForCredentials() throws IOException {
+        String[] tuple = new String[2];
+        
+        out.print("Username: ");
+        tuple[0] = in.readLine();
+        
+        out.print("Password: ");
+        tuple[1] = in.readLine();
+
+        return tuple;
+    }
 }
