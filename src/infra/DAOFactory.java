96cdf4512a58eeb4f4b9e4f410e176f1ebb51ef2
samueldemoura <samueldemouramoreira@gmail.com>
2019-05-08 09:34:43 -0300

    Implement singleton project pattern in DAOFactory

--- src/infra/DAOFactory.java
+++ src/infra/DAOFactory.java
@@ -8,27 +8,37 @@ package infra;
 import util.InfraException;
 
 /**
+ * This factory allows selection of what will be used for persistence. Abstract
+ * Factory + Singleton design pattern.
  *
- * This factory allows selection of what will be used for persistence.
- * Abstract Factory design pattern.
  * @author samueldemoura
  */
 public abstract class DAOFactory {
+
     // Possible persistence types supported by this factory
     public static final int TEXTFILE = 1;
     public static final int INMEMORY = 2;
-    
+    private static DAOFactory textfile_dao = null;
+    private static DAOFactory inmemory_dao = null;
+
     public abstract UserDAO getUserDAO() throws InfraException;
-	public abstract PoliceReportDAO getPoliceReportDAO() throws InfraException;
-    
+
+    public abstract PoliceReportDAO getPoliceReportDAO() throws InfraException;
+
     public static DAOFactory getDAOFactory(int factory_type) {
         switch (factory_type) {
             case TEXTFILE:
-                return new TextFileDAOFactory();
+                if (textfile_dao == null) {
+                    textfile_dao = new TextFileDAOFactory();
+                }
+                return textfile_dao;
             case INMEMORY:
-                return new InMemoryDAOFactory();
+                if (inmemory_dao == null) {
+                    inmemory_dao = new InMemoryDAOFactory();
+                }
+                return inmemory_dao;
             default:
                 return null;
-            }
+        }
     }
 }
